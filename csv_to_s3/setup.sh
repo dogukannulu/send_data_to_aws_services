d8bc0f3b2fd7d67adb93dd0f5bcbb172a48f5c0b
Dogukan Ulu <dogukannulu@gmail.com>
2023-07-31 22:12:57 +0300

    modify setup.sh

--- csv_to_s3/setup.sh
+++ csv_to_s3/setup.sh
@@ -30,7 +30,7 @@ download_zip_file() {
     log_message "Downloading the zip file"
     mkdir -p "$project_dir"
     cd "$project_dir" || exit 1
-    wget -q https://github.com/dogukannulu/send_data_to_aws_services/csv_to_s3/raw/main/csv_to_s3.zip
+    wget -q https://github.com/dogukannulu/send_data_to_aws_services/raw/main/csv_to_s3/csv_to_s3.zip
 }
 
 # Function to unzip the files
