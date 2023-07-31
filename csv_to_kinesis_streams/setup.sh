cc0647759ef532842a1959ceba15ee09d486f5b8
Dogukan Ulu <dogukannulu@gmail.com>
2023-07-31 23:39:16 +0300

    wget -> curl

--- csv_to_kinesis_streams/setup.sh
+++ csv_to_kinesis_streams/setup.sh
@@ -30,7 +30,7 @@ download_zip_file() {
     log_message "Downloading the zip file"
     mkdir -p "$project_dir"
     cd "$project_dir" || exit 1
-    wget -q https://github.com/dogukannulu/send_data_to_aws_services/raw/main/csv_to_kinesis_streams/csv_to_kinesis_streams.zip
+    curl -L -O https://github.com/dogukannulu/send_data_to_aws_services/raw/main/csv_to_kinesis_streams/csv_to_kinesis_streams.zip
 }
 
 # Function to unzip the files
